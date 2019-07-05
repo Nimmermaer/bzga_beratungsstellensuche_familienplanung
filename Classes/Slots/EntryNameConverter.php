618f0f9da47016a0423b5d023e7593b718d08208
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2019-07-05 22:17:13 +0200

    [BUGFIX] Change name for xml node for contraceptive_costs field

--- Classes/Slots/EntryNameConverter.php
+++ Classes/Slots/EntryNameConverter.php
@@ -31,7 +31,7 @@ class EntryNameConverter
             'konfession' => 'religious_denomination',
             'pndberatunglangsons' => 'pnd_other_language',
             'mutterundkind' => 'mother_and_child',
-            'kostenuebernahme_verhuetung' => 'contraceptive_costs',
+            'kostenuebernahmeverhuetung' => 'contraceptive_costs',
             'beratungsschein' => 'consulting_agreement',
             'pndberatunglang' => 'pnd_languages',
             'pndberatung' => 'pnd_consultings',
