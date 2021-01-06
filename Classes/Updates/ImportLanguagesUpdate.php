621c228870e10e48e7b1b1e3abdd30a496dacbe0
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2021-01-06 17:44:44 +0100

    [TASK] Fix functional tests

--- Classes/Updates/ImportLanguagesUpdate.php
+++ Classes/Updates/ImportLanguagesUpdate.php
@@ -1,9 +1,15 @@
 <?php
+
 declare(strict_types=1);
 
+/*
+ * This file is part of the "bzga_beratungsstellensuche_familienplanung" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
 
 namespace Bzga\BzgaBeratungsstellensucheFamilienplanung\Updates;
-
 
 use SJBR\StaticInfoTables\Utility\DatabaseUpdateUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -12,7 +18,6 @@ use TYPO3\CMS\Install\Updates\UpgradeWizardInterface;
 
 final class ImportLanguagesUpdate implements UpgradeWizardInterface
 {
-
     public function getIdentifier(): string
     {
         return self::class;
