60c57588c6509996b20db4cdf8f502055f531757
Michael Blunck <michael.blunck@webicorns.com>
2024-10-07 17:22:10 +0200

    [TASK] adds leafleat and javascript-builder as dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,11 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'ssch@hauptweg-nebenwege.de',
     'author_company' => 'Hauptweg Nebenwege GmbH',
     'state' => 'beta',
-    'clearCacheOnLoad' => 0,
     'version' => '11.5.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-11.5.99',
+            'typo3' => '12.4.0-12.4.99',
             'bzga_beratungsstellensuche' => '8.7.0-',
         ],
         'conflicts' => [],
